f23a9a61e3a3d4982c07ad2660c62b99263842f1
Young.liu <onlysuncolour@sina.com>
2022-02-15 09:09:44 +0800

    fix: rm time in data

--- gameData/loadData.js
+++ gameData/loadData.js
@@ -2,20 +2,29 @@ const {httpGet} = require('../utils/httpGet')
 const {writeFile} = require('../utils/writeFile')
 
 let page =1, max, result = [],
-  type="skill" // card , skill , contract , rune 
+  type="contract" // card , skill , contract , rune 
 let makeUrl = (_page = page, _type=type) => {
   return `https://wiki.mysticalcard.com/mysticalcard/v2/data/indices/?keyword=&server=x&type=${_type}&page=${_page}`
 }
+const pushDatas = data => {
+  const results = []
+  data.items.forEach(_d => {
+    delete _d.time
+    console.log(_d)
+    results.push(_d)
+  })
+  return results
+}
 httpGet(makeUrl()).then(data => {
   max = data.count;
-  result.push(...data.items);
+  result.push(...pushDatas(data));
   let promises = []
   while(page < max) {
     page++;
     promises.push(httpGet(makeUrl(page), page - 1))
   }
   Promise.all(promises).then(datas => {
-    datas.forEach(d => result.push(...d.items))
+    datas.forEach(d => pushDatas(d))
     writeFile(`gameData/fulldata/${type}.json`, JSON.stringify(result))
   })
 })
